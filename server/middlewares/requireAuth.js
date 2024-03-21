c6ba04ab6d82842e1839f1a6222db45fd305a888
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-03-21 13:20:36 +0530

    auth middleware

---
+++ server/middlewares/requireAuth.js
@@ -0,0 +1,24 @@
+const jwt= require('jsonwebtoken')
+const User=require('../models/User')
+
+const requireAuth= async(req,res,next)=>{
+   const {authorization}= req.header
+   if(!authorization){
+    res.status(401).json({error:"Authorization token required"})
+   }
+   //authorization =>     Bearer gshjdfgjhfgsjfhfigf.dsdyweyhdd.jadsagdajshd    so we split the first part to get token exactly
+   const token =authorization.split(' ')[1]
+
+   try {
+    const {_id}=jwt.verify(token,process.env.JWT_SECRET)     //we verify and extract _id from the token
+     req.user= await User.findOne({_id}).select('_id')   //we are attaching user property to req and passing it on to next middleware of main function and we can use that tha user property as we know now the user is authenticated
+     next()
+
+   } catch (error) {
+    console.log(error)
+    res.status(401).json({error:'Request not authorized'})
+   }
+   
+
+}
+module.exports=requireAuth
