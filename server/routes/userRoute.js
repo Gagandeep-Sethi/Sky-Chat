786a232f5de435304b2fe8a441080ae6a81c1355
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-18 21:14:49 +0530

    user route

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,15 @@
-const express=require("express")
-const { signup, login,search } = require("../controllers/userController")
-const requireAuth = require("../middlewares/requireAuth")
-const router=express.Router()
+const express = require("express");
+const {
+  signup,
+  login,
+  search,
+  logout,
+} = require("../controllers/userController");
+const requireAuth = require("../middlewares/requireAuth");
+const router = express.Router();
 
-router.post('/user/login',login)
-router.post('/user/signup',signup)
-router.get('/user?search=gagan',requireAuth, search)
-module.exports=router
+router.post("/user/login", login);
+router.post("/user/signup", signup);
+router.get("/user?search=gagan", requireAuth, search);
+router.get("/user/logout", logout);
+module.exports = router;
