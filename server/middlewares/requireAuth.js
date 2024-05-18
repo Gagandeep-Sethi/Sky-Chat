a68414f8e28952e93d71d42b7d80fc505bcf7a7f
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-18 23:43:49 +0530

    require auth middleware

--- server/middlewares/requireAuth.js
+++ server/middlewares/requireAuth.js
@@ -1,27 +1,28 @@
-const jwt= require('jsonwebtoken')
-const User=require('../models/User')
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-const requireAuth= async(req,res,next)=>{
-   const {authorization}= req.header
-   if(!authorization){
-    res.status(401).json({error:"Authorization token required"})
-   }
-   //authorization =>     Bearer gshjdfgjhfgsjfhfigf.dsdyweyhdd.jadsagdajshd    
-   const token =authorization.split(' ')[1]     //so we split the first part to get token exactly
-
-   try {
-    const {_id}=jwt.verify(token,process.env.JWT_SECRET)     //we verify and extract _id from the token
-     req.user= await User.findOne({_id}).select('_id')   //we are attaching user property to req and passing it on to next middleware of main function and we can use that tha user property as we know now the user is authenticated
-     next()
-
-   } catch (error) {
-    console.log(error)
-    res.status(401).json({error:'Request not authorized'})
-   }
-   
-
-}
-module.exports=requireAuth
+const requireAuth = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+    if (!token) {
+      res.status(401).json({ message: "unauthorised token not found" });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      res.status(401).json({ message: "unauthorised invalid token" });
+    }
+    const user = await User.findById(decoded._id).select("-password");
+    if (!user) {
+      res.status(400).json({ message: "user not found" });
+    }
+    req.user = user; //we are attaching user property to req and passing it on to next middleware of main function and we can use that tha user property as we know now the user is authenticated
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Request not authorized" });
+  }
+};
+module.exports = requireAuth;
 
 // to send this header
 //const user= useSeelector(...........)  user present in store
@@ -29,7 +30,7 @@ module.exports=requireAuth
 //    const data=await fetch('/api/workout',
 //    {
 //       headers:{
-//          'Authorization': `Bearer ${user.token}`      //assigning auth header with req       
+//          'Authorization': `Bearer ${user.token}`      //assigning auth header with req
 //       }
 //    })
 // if(data.ok){
