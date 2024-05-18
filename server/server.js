055313e160a7d6d518af18a9b0ab1ebd079aecb0
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-18 21:14:34 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,26 +1,26 @@
-const express= require ("express")
-const userRouter=require('./routes/userRoute')
-const mongoose= require("mongoose")
+const express = require("express");
+const userRouter = require("./routes/userRoute");
+const mongoose = require("mongoose");
 
-const googleRouter = require('./routes/googleRoute')
-require("dotenv").config()
-const app=express()
+const googleRouter = require("./routes/googleRoute");
+require("dotenv").config();
+const app = express();
 
+app.use(express.json());
 
-app.use(express.json())
-
-const passport = require('passport');
-require('./config/passport');
-mongoose.connect(process.env.MONGO_DB_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+const passport = require("passport");
+require("./config/passport");
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-})
-app.use('/api',userRouter)
-app.use('/auth/google', googleRouter);
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+app.use("/api", userRouter);
+app.use("/auth/google", googleRouter);
 
-app.listen(process.env.PORT,()=>console.log("server listening at port 5000"))
-
+app.listen(process.env.PORT, () =>
+  console.log("server listening at port 5000")
+);
