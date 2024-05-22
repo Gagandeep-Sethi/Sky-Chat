23763447d1d55586f725bab57dc557e01e9c585a
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-22 18:39:38 +0530

    group chat

--- server/models/ChatMessage.js
+++ server/models/ChatMessage.js
@@ -1,22 +1,66 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const chatSchema=new mongoose.Schema({
-chatName:{type:String,trim:true},
-isGroupChat:{type:Boolean,default:false},
-users:[
-    {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'user'
-    }
-],
-latestMessage:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'message'
-},
-groupAdmin:{type:mongoose.Schema.Types.ObjectId,ref:'user'}
-},
-{
-    timestamps:true
-})
+const chatSchema = new mongoose.Schema(
+  {
+    chatName: {
+      type: String,
+      trim: true,
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports=mongoose.model('chat',chatSchema)
+module.exports = mongoose.model("Chat", chatSchema);
+
+// const mongoose = require("mongoose");
+
+// const chatSchema = new mongoose.Schema(
+//   {
+//     chatName: { type: String, trim: true },
+//     isGroupChat: { type: Boolean, default: false },
+//     users: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User",
+//       },
+//     ],
+//     latestMessage: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Message",
+//     },
+//     chat: [
+//       {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Message",
+//         default: [],
+//       },
+//     ],
+//     groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// module.exports = mongoose.model("Chat", chatSchema);
--- server/models/Message.js
+++ server/models/Message.js
@@ -1,12 +1,55 @@
-const mongoose= require('mongoose')
+const mongoose = require("mongoose");
 
-const messageSchema= new mongoose.Schema({
-sender:{type:mongoose.Schema.Types.ObjectId, ref:'user'},
-content:{type:String, trim:true},//it removes any white space from the begining and the last
-chat:{type:mongoose.Schema.Types.ObjectId ,ref:'chat'}
-},
-{
-    timestamps:true
-})
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    chatId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    content: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    readBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports=mongoose.model('message',messageSchema)
+module.exports = mongoose.model("Message", messageSchema);
+
+// const mongoose = require("mongoose");
+
+// const messageSchema = new mongoose.Schema(
+//   {
+//     senderId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     receiverId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     message: { type: String, trim: true, required: true }, //it removes any white space from the begining and the last
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// module.exports = mongoose.model("Message", messageSchema);
