d4bdcc2293a0363d3b1eae506ca5a580f2e729cc
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-24 13:33:03 +0530

    email setup

---
+++ server/helpers/email.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+const sendEmail = async (email, subject, text, html) => {
+  try {
+    let info = await transporter.sendMail({
+      from: {
+        name: "Sky-Shop",
+        address: process.env.EMAIL_USER,
+      },
+      to: email,
+      subject: subject,
+      text: text,
+      html: html,
+    });
+  } catch (error) {
+    console.log(error, "sending mail error");
+  }
+};
+
+module.exports = {
+  sendEmail,
+};
