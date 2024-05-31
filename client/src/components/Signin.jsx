b7bdd16ac76dc4d506d8ed3715516e60fe76feb0
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-05-31 23:38:58 +0530

    signin

--- client/src/components/Signin.jsx
+++ client/src/components/Signin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FiEye, FiEyeOff } from "react-icons/fi";
-
+import { useLogin } from "../utils/hooks/useLogin";
 const Signin = () => {
   const [showPassword, setShowPassword] = useState(false);
 
@@ -8,9 +8,26 @@ const Signin = () => {
     setShowPassword(!showPassword);
   };
 
+  const [formValue, setFormValue] = useState({
+    password: "",
+    email: "",
+  });
+  const { login, isLoading, error } = useLogin();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(formValue);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValue((prevFormValue) => ({
+      ...prevFormValue,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="w-full h-full md:flex  rounded-2xl overflow-hidden">
-      <div className="w-full md:w-1/2 md:h-full h-[40%]  bg-[#cad2c5] relative items-center flex ">
+      <div className="w-full md:w-1/2 md:h-full h-[40%]   bg-customGreen relative items-center flex ">
         <img
           src="/Logo.png"
           alt=""
@@ -26,9 +43,15 @@ const Signin = () => {
       <div className="w-full md:w-1/2  h-[60%] md:h-full    bg-customSkin   flex flex-col justify-center items-center">
         <h1 className="font-mono text-3xl mb-10 text-customCyan">Login</h1>
         <div>
-          <form className="md:space-y-8 space-y-6 w-full h-full flex flex-col items-center  ">
+          <form
+            onSubmit={handleSubmit}
+            className="md:space-y-8 space-y-6 w-full h-full flex flex-col items-center  "
+          >
             <div class="relative">
               <input
+                name="email"
+                value={formValue.email}
+                onChange={handleChange}
                 required
                 id="email"
                 className="peer text-slate-600 md:w-64 w-52 bg-customSkin rounded-lg border border-customCyan px-2 py-1 placeholder-transparent placeholder-shown:border-gray-500 focus:border-customCyan focus:outline-none "
@@ -36,15 +59,18 @@ const Signin = () => {
                 placeholder="enter your email"
               />
               <label
-                for="email"
+                htmlFor="email"
                 className="absolute left-2 top-1.5 cursor-text font-extralight text-sm text-gray-400 transition-all duration-[250ms] peer-valid:-top-3 peer-valid:bg-customSkin peer-valid:text-sm peer-valid:text-customCyan peer-focus:-top-3 peer-focus:bg-customSkin peer-focus:text-sm peer-focus:text-customCyan"
               >
                 Enter your email
                 <span className="text-red-600 ">*</span>
               </label>
             </div>
-            <div class="relative">
+            <div class="relative ">
               <input
+                name="password"
+                value={formValue.password}
+                onChange={handleChange}
                 required
                 id="password"
                 className="peer text-slate-600 md:w-64 w-52  bg-customSkin rounded-lg border border-customCyan px-2 py-1 placeholder-transparent placeholder-shown:border-gray-500 focus:border-customCyan focus:outline-none "
@@ -67,15 +93,26 @@ const Signin = () => {
                 )}
               </button>
               <label
-                for="password"
+                htmlFor="password"
                 className="absolute left-2 top-1.5 cursor-text font-extralight text-sm text-gray-400 transition-all duration-[250ms] peer-valid:-top-3 peer-valid:bg-customSkin peer-valid:text-sm peer-valid:text-customCyan peer-focus:-top-3 peer-focus:bg-customSkin peer-focus:text-sm peer-focus:text-customCyan"
               >
                 Password
                 <span className="text-red-600 ">*</span>
               </label>
+              {error && (
+                <p className="text-red-600  text-center mt-3  text-sm  animate-bounce">
+                  {error} !!
+                </p>
+              )}
             </div>
 
-            <button className="btn btn-md   text-white hover:bg-cyan-700 bg-customCyan font-medium border-none rounded-3xl px-6 ">
+            <button
+              disabled={isLoading}
+              className="btn btn-md   text-white hover:bg-cyan-700 bg-customCyan font-medium border-none rounded-3xl px-6 "
+            >
+              {isLoading ? (
+                <span className="loading loading-spinner loading-xs"></span>
+              ) : null}
               Signin
             </button>
           </form>
