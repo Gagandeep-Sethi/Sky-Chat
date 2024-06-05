493eb1a5f79ccc8706827aeb8634a91c1e3807b3
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-06-05 23:31:51 +0530

    friend profile

---
+++ client/src/components/FriendList.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import MessageContainer from "./MessageContainer";
+import { Fetch_Uri } from "../utils/constants";
+
+const FriendList = () => {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(`${Fetch_Uri}/api/user/getFriends`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include", //this will let it set cookie
+      });
+      const json = await response.json();
+      console.log(json, "json");
+      setList(json);
+    }
+    getData();
+  }, []);
+  return (
+    <div>
+      <p className="text-lg px-4 py-3">All Chat</p>
+      {list.length > 0 ? <MessageContainer list={list} /> : null}
+    </div>
+  );
+};
+
+export default FriendList;
