3c087e890d499c788c9947e22ccc7ce83a9c780b
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-06-06 16:56:11 +0530

    add friend

---
+++ client/src/components/FriendProfile.jsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useState } from "react";
+import { FaArrowLeft } from "react-icons/fa6";
+import { useDispatch, useSelector } from "react-redux";
+import { setActiveComponent } from "../redux/uiSlice";
+import { useAddFriend } from "../utils/hooks/useAddFriend";
+
+const FriendProfile = () => {
+  const [profileDetails, setProfileDetails] = useState({
+    username: "",
+    profilePic: "",
+  });
+  const chat = useSelector((store) => store?.ui);
+  const dispatch = useDispatch();
+  const { addFriend, isLoading } = useAddFriend();
+
+  useEffect(() => {
+    if (chat?.profile === "friend") {
+      return setProfileDetails({
+        username: chat?.selectedChat.username,
+        profilePic: chat?.selectedChat.profilePic,
+      });
+    }
+  }, [chat]);
+  const handleArrowClicked = () => {
+    dispatch(setActiveComponent("chat"));
+  };
+  const handleAddFriend = async () => {
+    console.log(chat?.selectedChat?.FriendId, "iddd");
+    await addFriend(chat?.selectedChat?.FriendId);
+  };
+
+  return (
+    <div className=" transition-all duration-500 it relative w-full h-full items-center scrollbar-none overflow-y-auto overflow-x-hidden md:border-r border-gray-400">
+      <div className=" flex gap-6  justify-center items-center w-full md:py-3 pt-2.5 pb-1.5 sticky bg-neutral-900 top-0  z-10 ">
+        <FaArrowLeft
+          onClick={handleArrowClicked}
+          className=" w-6 h-6 absolute left-4 text-white cursor-pointer"
+        />
+        <p className="text-lg cursor-pointer ">Profile</p>
+      </div>
+      <div className="flex flex-col justify-center items-center pt-8">
+        <div className="avatar">
+          <div className="w-56 rounded-full">
+            <img
+              src={`https://res.cloudinary.com/dyja4tbmu/${profileDetails?.profilePic}.jpg`}
+              alt=""
+            />
+          </div>
+        </div>
+        <p className="text-xl py-3">{profileDetails?.username}</p>
+        <div>
+          <div>
+            <button
+              onClick={handleAddFriend}
+              className="btn   bg-blue-600 text-white hover:bg-blue-500"
+            >
+              Add friend
+            </button>
+            <button className="btn  bg-blue-600 text-white hover:bg-blue-500">
+              Remove friend
+            </button>
+          </div>
+          <div className="flex justify-center ">
+            <button className="btn  bg-blue-600 text-white hover:bg-blue-500">
+              Block User
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FriendProfile;
+
+// import React, { useEffect, useState } from "react";
+// import { FaArrowLeft } from "react-icons/fa6";
+// import { useSelector } from "react-redux";
+
+// const FriendProfile = () => {
+//   const [profileDetails, setProfileDetails] = useState({
+//     username: "",
+//     profilePic: "",
+//     email: "",
+//   });
+//   const chat = useSelector((store) => store?.ui);
+//   const user = useSelector((store) => store?.user);
+
+//   useEffect(() => {
+//     if (chat?.profile === "friend") {
+//       console.log("yes");
+//       return setProfileDetails({
+//         username: chat?.selectedChat.username,
+//         profilePic: chat?.selectedChat.profilePic,
+//       });
+//     } else if (chat?.profile === "own") {
+//       setProfileDetails({
+//         username: user?.username,
+//         profilePic: user?.profilePic,
+//         email: user?.email,
+//       });
+//     }
+//   }, [chat, user]);
+
+//   return (
+//     <div className=" transition-all duration-500 it relative w-full h-full items-center scrollbar-none overflow-y-auto overflow-x-hidden md:border-r border-gray-400">
+//       <div className=" flex gap-6  justify-center items-center w-full md:py-3 pt-2.5 pb-1.5 sticky bg-neutral-900 top-0  z-10 ">
+//         <FaArrowLeft className=" w-6 h-6 absolute left-4 text-white cursor-pointer" />
+//         <p className="text-lg cursor-pointer ">Profile</p>
+//       </div>
+//       <div className="flex flex-col justify-center items-center pt-8">
+//         <div className="avatar">
+//           <div className="w-56 rounded-full">
+//             <img
+//               src={`https://res.cloudinary.com/dyja4tbmu/${profileDetails?.profilePic}.jpg`}
+//               alt=""
+//             />
+//           </div>
+//         </div>
+//         <p className="text-xl py-3">{profileDetails?.username}</p>
+//         <div>
+//           <div>
+//             <button className="btn   bg-blue-600 text-white hover:bg-blue-500">
+//               Add friend
+//             </button>
+//             <button className="btn  bg-blue-600 text-white hover:bg-blue-500">
+//               Remove friend
+//             </button>
+//           </div>
+//           <div className="flex justify-center ">
+//             <button className="btn  bg-blue-600 text-white hover:bg-blue-500">
+//               Block User
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FriendProfile;
---
+++ client/src/utils/hooks/useAddFriend.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Fetch_Uri } from "../constants";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { setFriends } from "../../redux/userRelationsSlice";
+
+export const useAddFriend = () => {
+  const [isLoading, setIsLoading] = useState(null);
+  const dispatch = useDispatch();
+  const addFriend = async (id) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`${Fetch_Uri}/api/user/addFriend/${id}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(),
+        credentials: "include", //this will let it set cookie
+      });
+      const json = await response.json();
+
+      if (!response.ok) {
+        console.log(json, "error json");
+        toast.error(json?.message);
+        setIsLoading(false);
+      }
+      if (response.ok) {
+        console.log(json, " json");
+        dispatch(setFriends(json?.friends));
+        setIsLoading(false);
+      }
+    } catch (error) {}
+  };
+  return { addFriend, isLoading };
+};
