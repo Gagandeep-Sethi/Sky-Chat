8d22ca429ea10f1ce520ebc1c0361cb2a7b40228
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-06-09 23:31:12 +0530

    add firned and create group

---
+++ client/src/components/AddDropdown.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useRef, useEffect } from "react";
+import { LuPlusCircle } from "react-icons/lu";
+import { HiUserGroup } from "react-icons/hi2";
+import { PiUserCirclePlusFill } from "react-icons/pi";
+import { useDispatch } from "react-redux";
+import { setActiveComponent } from "../redux/uiSlice";
+import { RxCross2 } from "react-icons/rx";
+
+const AddDropdown = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+  function handleAddFriendClicked() {
+    setIsOpen(false);
+    dispatch(setActiveComponent("addFriend"));
+  }
+  function handleCreateGroupClicked() {
+    setIsOpen(false);
+    dispatch(setActiveComponent("createGroup"));
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="relative inline-block text-left" ref={dropdownRef}>
+      <button
+        onClick={toggleDropdown}
+        className="inline-flex transition-all duration-500 justify-center rounded-full w-full p-2 bg-green-500 "
+      >
+        {isOpen ? (
+          <RxCross2 className="w-7 h-7 text-black" />
+        ) : (
+          <LuPlusCircle className="w-7 h-7 text-black" />
+        )}
+      </button>
+      {isOpen && (
+        <div className="absolute right-9 z-10 mb-2 w-48 bottom-6 bg-white border border-gray-300 rounded-md shadow-lg">
+          <div
+            className="py-1 items-center flex flex-col justify-center"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="options-menu"
+          >
+            <div className="mx-auto w-44 hover:bg-gray-300 rounded-xl">
+              <button
+                onClick={handleAddFriendClicked}
+                className="items-center flex mx-auto   py-2 gap-2   text-gray-700 "
+                role="menuitem"
+                //onClick={() => setIsOpen(false)}
+              >
+                <PiUserCirclePlusFill className="text-black w-5 h-5" /> Add
+                friend
+              </button>
+            </div>
+            <div className="mx-auto w-44 hover:bg-gray-300 rounded-xl ">
+              <button
+                onClick={handleCreateGroupClicked}
+                className="items-center flex mx-auto   py-2 gap-2   text-gray-700 "
+                role="menuitem"
+                //onClick={() => setIsOpen(false)}
+              >
+                <HiUserGroup className="text-black w-5 h-5" /> Create group
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AddDropdown;
