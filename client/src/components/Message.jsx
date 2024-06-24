25166e3fa1b732d7491bace8a2534b7c5674a627
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-06-24 10:52:44 +0530

    container

---
+++ client/src/components/Message.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { formatChatTime } from "../utils/helpers/functions";
+import { useDispatch, useSelector } from "react-redux";
+import { setActiveComponent, setSelectedChat } from "../redux/uiSlice";
+
+const Message = ({ msg, type }) => {
+  const dispatch = useDispatch();
+  const { isDarkMode } = useSelector((state) => state.theme);
+
+  const handleChatSelect = (FriendId) => {
+    dispatch(setSelectedChat(FriendId));
+    dispatch(setActiveComponent(type));
+  };
+
+  return (
+    <div
+      onClick={() =>
+        handleChatSelect({
+          FriendId: msg?.friendId ? msg?.friendId : msg?.groupId,
+          username: msg?.username ? msg?.username : msg?.chatName,
+          profilePic: msg?.profilePic,
+          isGroupchat: msg?.friendId ? false : true,
+        })
+      }
+      className=" w-full cursor-pointer border-dashed border-b border-b-gray-600    px-1 py-2 flex gap-4 items-center  overflow-hidden "
+    >
+      <div className="avatar relative ">
+        <div className="w-14 h-14 rounded-full">
+          <img
+            src={`https://res.cloudinary.com/dyja4tbmu/${msg?.profilePic}.jpg`}
+            alt=""
+          />
+        </div>
+        <div className="bg-green-500 absolute rounded-full border-white border w-3 right-0"></div>
+      </div>
+
+      <div className="flex justify-between flex-1 items-center">
+        <div>
+          <p
+            className={` text-lg ${
+              isDarkMode ? "text-darkText1" : "text-lightText1"
+            }`}
+          >
+            {msg?.username ? msg?.username : msg?.chatName}
+          </p>
+          <p className="text-zinc-400 font-thin text-sm  font truncate text-ellipsis w-40">
+            {msg?.latestMessage}
+          </p>
+        </div>
+        <div>
+          <p className=" text-xs  text-zinc-400">
+            {msg?.chatTime ? formatChatTime(msg?.chatTime) : null}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
--- client/src/components/MessageContainer.jsx
+++ client/src/components/MessageContainer.jsx
@@ -1,56 +1,25 @@
 import React from "react";
 import Message from "./Message";
 
-const MessageContainer = ({ list }) => {
+const MessageContainer = ({ list, type }) => {
   console.log(list, "list");
   return (
     <div className="w-full h-full    overflow-y-auto space-y-3 ">
-      {list.map((res, i) => {
-        return (
-          <div key={i} className="w-full    ">
-            <div className="w-[92%] mx-auto transition-all duration-200 rounded-lg  :bg-blue-300">
-              <Message msg={res} />
+      {list.length === 0 ? (
+        <div className="flex items-center justify-center  text-center text-gray-500">
+          Add Friends to start chatting
+        </div>
+      ) : (
+        list.map((res, i) => {
+          return (
+            <div key={i} className="w-full    ">
+              <div className="w-full mx-auto transition-all duration-200 rounded-lg  :bg-blue-300">
+                <Message msg={res} type={type} />
+              </div>
             </div>
-          </div>
-        );
-      })}
-
-      {list.map((res, i) => {
-        return (
-          <div key={i} className="w-full flex   ">
-            <div className="w-[92%] mx-auto ">
-              <Message msg={res} />
-            </div>
-          </div>
-        );
-      })}
-      {list.map((res, i) => {
-        return (
-          <div key={i} className="w-full flex   ">
-            <div className="w-[92%] mx-auto">
-              <Message msg={res} />
-            </div>
-          </div>
-        );
-      })}
-      {list.map((res, i) => {
-        return (
-          <div key={i} className="w-full flex   ">
-            <div className="w-[92%] mx-auto">
-              <Message msg={res} />
-            </div>
-          </div>
-        );
-      })}
-      {list.map((res, i) => {
-        return (
-          <div key={i} className="w-full flex   ">
-            <div className="w-[92%] mx-auto">
-              <Message msg={res} />
-            </div>
-          </div>
-        );
-      })}
+          );
+        })
+      )}
     </div>
   );
 };
