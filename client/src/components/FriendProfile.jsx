a12367fe29d94bd665e0b6f7469d1eff483876ae
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-06-24 18:23:46 +0530

    profile button ui

--- client/src/components/FriendProfile.jsx
+++ client/src/components/FriendProfile.jsx
@@ -12,6 +12,8 @@ const FriendProfile = () => {
     username: "",
     profilePic: "",
   });
+  const [isFriend, setIsFriend] = useState(true);
+  const [isBlocked, setIsBlocked] = useState(false);
   const chat = useSelector((store) => store?.ui);
   const dispatch = useDispatch();
   const { addFriend, isLoadingAdd } = useAddFriend();
@@ -19,6 +21,7 @@ const FriendProfile = () => {
   const { block, isLoadingBlock } = useBlockFriend();
   const { UnBlock, isLoadingUnBlock } = useUnBlockFriend();
   const { isDarkMode } = useSelector((state) => state.theme);
+  const { friends, blocked } = useSelector((store) => store?.userRelations);
 
   useEffect(() => {
     if (chat?.profile === "friend") {
@@ -28,6 +31,20 @@ const FriendProfile = () => {
       });
     }
   }, [chat]);
+  useEffect(() => {
+    if (friends.includes(chat?.selectedChat?.FriendId)) {
+      setIsFriend(true);
+    } else {
+      setIsFriend(false);
+    }
+  }, [chat?.selectedChat?.FriendId, friends]);
+  useEffect(() => {
+    if (blocked.includes(chat?.selectedChat?.FriendId)) {
+      setIsBlocked(true);
+    } else {
+      setIsBlocked(false);
+    }
+  }, [chat?.selectedChat?.FriendId, blocked]);
   const handleArrowClicked = () => {
     dispatch(setActiveComponent("chat"));
   };
@@ -82,40 +99,46 @@ const FriendProfile = () => {
           {profileDetails?.username}
         </p>
         <div>
-          <div>
-            <button
-              disabled={isLoadingAdd}
-              onClick={handleAddFriend}
-              className="btn   bg-blue-600 text-white hover:bg-blue-500"
-            >
-              {isLoadingAdd ? (
-                <span className="loading loading-spinner loading-xs"></span>
-              ) : null}
-              Add friend
-            </button>
-            <button
-              onClick={handleRemoveFriend}
-              disabled={isLoadingRemove}
-              className="btn  bg-blue-600 text-white hover:bg-blue-500"
-            >
-              Remove friend
-            </button>
+          <div className="flex justify-center">
+            {!isFriend ? (
+              <button
+                disabled={isLoadingAdd}
+                onClick={handleAddFriend}
+                className="btn   bg-blue-600 text-white hover:bg-blue-500"
+              >
+                {isLoadingAdd ? (
+                  <span className="loading loading-spinner loading-xs"></span>
+                ) : null}
+                Add friend
+              </button>
+            ) : (
+              <button
+                onClick={handleRemoveFriend}
+                disabled={isLoadingRemove}
+                className="btn  bg-blue-600 text-white hover:bg-blue-500"
+              >
+                Remove friend
+              </button>
+            )}
           </div>
           <div className="flex justify-center ">
-            <button
-              disabled={isLoadingBlock}
-              onClick={handleBlockFriend}
-              className="btn  bg-blue-600 text-white hover:bg-blue-500"
-            >
-              Block User
-            </button>
-            <button
-              disabled={isLoadingUnBlock}
-              onClick={handleUnBlockFriend}
-              className="btn  bg-blue-600 text-white hover:bg-blue-500"
-            >
-              Unblock User
-            </button>
+            {!isBlocked ? (
+              <button
+                disabled={isLoadingBlock}
+                onClick={handleBlockFriend}
+                className="btn  bg-blue-600 text-white hover:bg-blue-500"
+              >
+                Block User
+              </button>
+            ) : (
+              <button
+                disabled={isLoadingUnBlock}
+                onClick={handleUnBlockFriend}
+                className="btn  bg-blue-600 text-white hover:bg-blue-500"
+              >
+                Unblock User
+              </button>
+            )}
           </div>
         </div>
       </div>
@@ -124,70 +147,3 @@ const FriendProfile = () => {
 };
 
 export default FriendProfile;
-
-// import React, { useEffect, useState } from "react";
-// import { FaArrowLeft } from "react-icons/fa6";
-// import { useSelector } from "react-redux";
-
-// const FriendProfile = () => {
-//   const [profileDetails, setProfileDetails] = useState({
-//     username: "",
-//     profilePic: "",
-//     email: "",
-//   });
-//   const chat = useSelector((store) => store?.ui);
-//   const user = useSelector((store) => store?.user);
-
-//   useEffect(() => {
-//     if (chat?.profile === "friend") {
-//       console.log("yes");
-//       return setProfileDetails({
-//         username: chat?.selectedChat.username,
-//         profilePic: chat?.selectedChat.profilePic,
-//       });
-//     } else if (chat?.profile === "own") {
-//       setProfileDetails({
-//         username: user?.username,
-//         profilePic: user?.profilePic,
-//         email: user?.email,
-//       });
-//     }
-//   }, [chat, user]);
-
-//   return (
-//     <div className=" transition-all duration-500 it relative w-full h-full items-center scrollbar-none overflow-y-auto overflow-x-hidden md:border-r border-gray-400">
-//       <div className=" flex gap-6  justify-center items-center w-full md:py-3 pt-2.5 pb-1.5 sticky bg-neutral-900 top-0  z-10 ">
-//         <FaArrowLeft className=" w-6 h-6 absolute left-4 text-white cursor-pointer" />
-//         <p className="text-lg cursor-pointer ">Profile</p>
-//       </div>
-//       <div className="flex flex-col justify-center items-center pt-8">
-//         <div className="avatar">
-//           <div className="w-56 rounded-full">
-//             <img
-//               src={`https://res.cloudinary.com/dyja4tbmu/${profileDetails?.profilePic}.jpg`}
-//               alt=""
-//             />
-//           </div>
-//         </div>
-//         <p className="text-xl py-3">{profileDetails?.username}</p>
-//         <div>
-//           <div>
-//             <button className="btn   bg-blue-600 text-white hover:bg-blue-500">
-//               Add friend
-//             </button>
-//             <button className="btn  bg-blue-600 text-white hover:bg-blue-500">
-//               Remove friend
-//             </button>
-//           </div>
-//           <div className="flex justify-center ">
-//             <button className="btn  bg-blue-600 text-white hover:bg-blue-500">
-//               Block User
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FriendProfile;
