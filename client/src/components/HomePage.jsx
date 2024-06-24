14291a630a7882683cba65a5c11aeceb1f4790a5
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-06-24 10:42:45 +0530

    homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,31 +1,62 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "./Sidebar";
 import Conversation from "./Conversation";
-import FriendProfile from "./FriendProfile";
 import { useSelector } from "react-redux";
-import OwnProfile from "./OwnProfile";
+import AddFriends from "./AddFriends";
+import CreateGroup from "./CreateGroup";
+import ProfileContainer from "./ProfileContainer";
+import AddNewMembers from "./AddNewMembers";
+import socketService from "../utils/socketService";
 
 const HomePage = () => {
   const { activeComponent, selectedChat } = useSelector((state) => state.ui);
-  const profile = useSelector((store) => store?.ui?.profile);
+  const { isDarkMode } = useSelector((state) => state.theme);
 
   // const handleProfileClick = () => {
   //   dispatch(setActiveComponent("profile"));
   // };
+  useEffect(() => {
+    socketService.connect();
+  }, []);
 
   return (
-    <div className=" h-screen  w-screen flex justify-center items-center  bg-[url('./images/chatBackground.jpg')]">
-      <div className="flex w-full h-full  bg-neutral-900  border border-gray-100 overflow-hidden">
+    <div className={`h-screen  w-screen flex justify-center items-center  `}>
+      <div
+        className={`flex w-full h-full  ${
+          isDarkMode ? "bg-darkBg" : "bg-lightBg"
+        }  border border-gray-100 overflow-hidden`}
+      >
         <div
-          className={`w-full transition-width lg:w-96 ${
+          className={`w-full transition-transform lg:w-96 ${
             activeComponent === "sidebar" ? "block" : "hidden"
           } lg:block`}
         >
           <Sidebar />
         </div>
         <div
-          className={`flex-grow h-full transition-width ${
-            activeComponent === "chat" ? "block" : "hidden"
+          className={`transition-transform fixed w-full h-full lg:w-96 left-0 top-0 z-20  ${
+            activeComponent === "createGroup"
+              ? "transform translate-x-0 w-full lg:w-1/4"
+              : "hidden-profile"
+          }`}
+        >
+          <CreateGroup />
+        </div>
+
+        <div
+          className={`transition-transform fixed w-full h-full lg:w-96 left-0 top-0 z-20 ${
+            activeComponent === "addFriend"
+              ? "transform translate-x-0 w-full lg:w-1/4"
+              : "hidden-profile"
+          }`}
+        >
+          <AddFriends />
+        </div>
+        <div
+          className={`flex-grow h-full transition-width  ${
+            activeComponent === "chat" || activeComponent === "group"
+              ? "block"
+              : "hidden"
           } lg:block `}
         >
           {selectedChat?.username ? (
@@ -43,7 +74,16 @@ const HomePage = () => {
               : "hidden-profile"
           }`}
         >
-          {profile === "friend" ? <FriendProfile /> : <OwnProfile />}
+          <ProfileContainer />
+        </div>
+        <div
+          className={`transition-transform fixed lg:relative right-0 top-0 h-full ${
+            activeComponent === "addNewMembers"
+              ? "transform translate-x-0 w-full lg:w-1/4"
+              : "hidden-profile"
+          }`}
+        >
+          <AddNewMembers />
         </div>
       </div>
     </div>
