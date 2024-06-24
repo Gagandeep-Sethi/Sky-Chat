1c122f20359cbecd1b5d9b8c546a0e44cef022d4
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-06-24 10:56:59 +0530

    unblock friend

--- client/src/utils/hooks/useUnblockFriend.js
+++ client/src/utils/hooks/useUnblockFriend.js
@@ -2,37 +2,46 @@ import { useState } from "react";
 import { Fetch_Uri } from "../constants";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
-import { setBlocked } from "../../redux/userRelationsSlice";
+import {
+  clearBlocked,
+  clearFriends,
+  setBlocked,
+} from "../../redux/userRelationsSlice";
+import { fetchWrapper } from "../helpers/functions";
+import { removeUser } from "../../redux/userSlice";
 
 export const useUnBlockFriend = () => {
   const [isLoadingUnBlock, setIsLoading] = useState(null);
   const dispatch = useDispatch();
+
   const UnBlock = async (id) => {
     setIsLoading(true);
     try {
-      const response = await fetch(
+      const response = await fetchWrapper(
         `${Fetch_Uri}/api/user/removeBlockedFriend/${id}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(),
-          credentials: "include", //this will let it set cookie
         }
       );
-      const json = await response.json();
 
-      if (!response.ok) {
-        console.log(json, "error json");
-        toast.error(json?.message);
-        setIsLoading(false);
-      }
-      if (response.ok) {
-        console.log(json, " json");
+      if (response.unauthorized) {
+        dispatch(removeUser());
+        dispatch(clearFriends());
+        dispatch(clearBlocked());
+      } else if (response.error) {
+        toast.error(response.error?.message || "An error occurred");
+      } else {
         toast.success("User removed from blocked list");
-        dispatch(setBlocked(json?.blocked));
-        setIsLoading(false);
+        dispatch(setBlocked(response?.blocked));
       }
-    } catch (error) {}
+    } catch (error) {
+      console.error("Fetch error:", error);
+      toast.error("An error occurred");
+    } finally {
+      setIsLoading(false);
+    }
   };
   return { UnBlock, isLoadingUnBlock };
 };
