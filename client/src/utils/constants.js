c8e3daf3fb4a136b0c366e246a01290c64016c26
Gagandeep Sethi <gagandeepsethi.7895@gmail.com>
2024-07-11 21:01:20 +0530

    cors

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,2 @@
-//export const Fetch_Uri = "https://sky-chat-server.vercel.app";
-export const Fetch_Uri = "http://localhost:5000";
+export const Fetch_Uri = "https://sky-chat-server.vercel.app";
+//export const Fetch_Uri = "http://localhost:5000";
--- server/helpers/tokenGeneration.js
+++ server/helpers/tokenGeneration.js
@@ -30,7 +30,7 @@ const generateJwtToken = (_id, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-    sameSite: "lax", // 'none' for cross-domain cookies
+    sameSite: "none", // 'none' for cross-domain cookies
     secure: true, // ensure this is true if your site is HTTPS
   });
 };
